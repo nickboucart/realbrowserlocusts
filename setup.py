820e02b2f84194f62c2bbad402d17b700e3559d2
Nick Boucart <nick.boucart@sirris.be>
2017-10-05 10:14:27 +0200

    Version bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'realbrowserlocusts',
   packages = ['realbrowserlocusts'],
-  version = '0.3',
+  version = '0.4',
   description = 'Minimal set of real browser (Firefox, Chrome and PhantomJS) locusts to be used in conjuntion with locust.io',
   author = 'Nick Bocuart',
   author_email = 'nboucart@gmail.com',
